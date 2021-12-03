ba41e8e12d09a4025baff0d55be4590a16c72b80
C.Tabanera <carlosrodrigueztabanera@gmail.com>
2021-12-03 12:22:48 +0100

    modificado clase Test

--- Tercero/src/test/MainTest.java
+++ Tercero/src/test/MainTest.java
@@ -2,6 +2,7 @@ package test;
 import java.util.*;
 
 import com.java.main.JugadorSeleccion;
+import com.sun.source.tree.AssertTree;
 import org.junit.*;
 
 public class MainTest  {
@@ -21,11 +22,24 @@ public class MainTest  {
 
     }
     @Test
-    public void longitudMapa() {
+    public void longitudMapaTest() {
         Map <String, JugadorSeleccion> jugadores = new TreeMap<>();
         jugadores.put("Busquets", new JugadorSeleccion(16, "Busquets", "Medio Centro"));
         Assert.assertEquals(1, jugadores.size());
 
     }
 
+    @Test
+    public void testAssertMap() {
+        Map <String, JugadorSeleccion> jugadores = new TreeMap<>();
+        mapIsEmptyTest();
+        jugadores.put("Casillas", new JugadorSeleccion(1, "Casillas", "Portero"));
+        jugadores.put("Ramos", new JugadorSeleccion(15, "Ramos", "Lateral Derecho"));
+        jugadores.put("Pique", new JugadorSeleccion(13, "Pique", "Central"));
+        jugadores.put("Puyol", new JugadorSeleccion(5, "Puyol", "Central"));
+        jugadores.put("Capdevila", new JugadorSeleccion(11, "Capdevila", "Lateral Izquierdo"));
+        Assert.assertNotNull(jugadores);
+        Assert.assertTrue("Casillas", jugadores.containsKey("Casillas"));
+
+    }
 }
